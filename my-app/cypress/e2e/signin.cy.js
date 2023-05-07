9b421c69e3136cd0ab697dd043d03757cd411aef
Ioana Lazea <lazeaioanabianca@gmail.com>
2023-05-07 14:47:57 +0300

    Testing sign in errors

--- my-app/cypress/e2e/signin.cy.js
+++ my-app/cypress/e2e/signin.cy.js
@@ -1,8 +1,8 @@
 describe('Visit website', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/');
   })
-})
+});
 
 describe('Log in', () => {
   it('passes', () => {
@@ -18,7 +18,7 @@ describe('Log in', () => {
     cy.url().should('include', '/home');
     
   })
-})
+});
 
 describe('Go to forgot password', () => {
   it('passes', () => {
@@ -30,4 +30,47 @@ describe('Go to forgot password', () => {
     
     cy.contains('Go back').click();
   })
-})
+});
+
+describe('Error: empty fields', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('.button-style-blk').click();
+   
+    cy.contains('Empty fields!');
+  })
+});
+
+
+describe('Error: invalid e-mail', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('.MuiFormControl-root').first().type('aaaaa');
+
+    cy.get('.MuiFormControl-root').eq(1).type('aaaa');
+
+    cy.get('.button-style-blk').click();
+
+    cy.contains('Email is invalid!');
+
+  })
+});
+
+
+
+describe('Error: wrong password', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    // Get an input, type e-mail
+    cy.get('.MuiFormControl-root').first().type('lazeaioanabianca@gmail.com');
+
+    // Get an input, type password
+    cy.get('.MuiFormControl-root').eq(1).type('aaaaa');
+
+    cy.get('.button-style-blk').click();
+
+    cy.contains('Wrong password!');
+
+  })
+});
